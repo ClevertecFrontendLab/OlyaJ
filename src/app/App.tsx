89feaf3466cb9d335cd72cf7225d349c22a9ece3
OlyaJ <schastnayaolga92@gmail.com>
2025-04-15 15:21:23 +0300

    fix: fixes in components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,15 @@
 import './App.css';
 import './../index.css';
 
-import { useRoutes } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 import { ScrollToTop } from '~/components/ScrollToTop/ScrollToTop';
-import { routes } from '~/routes/Router';
 
 function App() {
-    const routing = useRoutes(routes);
-
     return (
         <>
             <ScrollToTop />
-            {routing}
+            <Outlet />
         </>
     );
 }
