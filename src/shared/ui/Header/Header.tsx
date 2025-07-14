055cb0bf60068ee402e881bded2dbe3cee8aa9a8
OlyaJ <schastnayaolga92@gmail.com>
2025-07-14 22:05:10 +0300

    header component

--- src/shared/ui/Header/Header.tsx
+++ src/shared/ui/Header/Header.tsx
@@ -1,6 +1,13 @@
-import { Box } from '@chakra-ui/react';
-import { headerStyles } from './header.styles';
+import { Box, Container, Image } from '@chakra-ui/react';
+import { containerStyles, headerStyles, imageStyles } from './header.styles';
+import logo from './../../../../public/logo.svg';
 
 export const Header = () => {
-    return <Box as='header' {...headerStyles}></Box>;
+    return (
+        <Box as='header' {...headerStyles}>
+            <Container {...containerStyles}>
+                <Image src={logo} alt='logo' {...imageStyles} />
+            </Container>
+        </Box>
+    );
 };
