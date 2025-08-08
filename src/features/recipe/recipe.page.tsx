8e2bcbdfb7710f4b0e40857ebeec9ae7d73ff485
OlyaJ <schastnayaolga92@gmail.com>
2025-08-08 20:46:48 +0300

    recipe page

--- src/features/recipe/recipe.page.tsx
+++ src/features/recipe/recipe.page.tsx
@@ -1,73 +1,115 @@
-import { Box, Image, Spinner, Text } from "@chakra-ui/react";
-import { categoriesIconsStyle, categoryBoxStyle, descriptionBoxStyle, descriptionRecipeStyle, imageRecipeStyle, pictureDescriptionBoxStyle, recipeCategoriesStyle, recipePageStyle, titleDescriptionBoxStyle, titleRecipeStyle } from "./recipe.styles";
-import { useParams } from "react-router-dom";
-import { useGetRecipeByIdQuery } from "./../../entities/recipes/api/recipesApi";
-import { Error } from "@shared/ui/Error/Error";
-import { BASE_URL } from "@shared/constants/api";
-import { getCategoryPairFromRecipe } from "@shared/lib/getCategoryPairFromRecipe";
-import { Recipe } from "src/entities/recipes/model/recipesTypes";
-import { useCategoryMap } from "@shared/hooks/useCategoryMap";
-import saveIcon from './../../../public/bookmarkHeart.svg'
-import likeIcon from './../../../public/heartEyes.svg'
-import { iconLikeSaveBoxStyle, iconStyle, likeSaveIconsBox } from "@shared/ui/Cards/NewRecipeDescktopCard/newRecipeDesktopCard.styles";
-
+import { Box, Button, HStack, Icon, Image, Spinner, Text } from '@chakra-ui/react';
+import {
+    buttonRecipeStyle,
+    buttonsBoxStyle,
+    categoriesIconsStyle,
+    categoryBoxStyle,
+    descriptionBoxStyle,
+    descriptionRecipeStyle,
+    imageRecipeStyle,
+    pictureDescriptionBoxStyle,
+    recipeCategoriesStyle,
+    recipePageStyle,
+    timeRecipeStyle,
+    titleDescriptionBoxStyle,
+    titleRecipeStyle,
+} from './recipe.styles';
+import { useParams } from 'react-router-dom';
+import { useGetRecipeByIdQuery } from './../../entities/recipes/api/recipesApi';
+import { Error } from '@shared/ui/Error/Error';
+import { BASE_URL } from '@shared/constants/api';
+import { getCategoryPairFromRecipe } from '@shared/lib/getCategoryPairFromRecipe';
+import { Recipe } from 'src/entities/recipes/model/recipesTypes';
+import { useCategoryMap } from '@shared/hooks/useCategoryMap';
+import saveIcon from './../../../public/bookmarkHeart.svg';
+import { BsBookmark } from 'react-icons/bs';
+import { MdSentimentSatisfiedAlt } from 'react-icons/md';
+import likeIcon from './../../../public/heartEyes.svg';
+import {
+    iconLikeSaveBoxStyle,
+    iconStyle,
+    likeSaveIconsBox,
+} from '@shared/ui/Cards/NewRecipeDescktopCard/newRecipeDesktopCard.styles';
 
 function RecipePage() {
-  const { recipeId } = useParams<{ recipeId?: string }>();
+    const { recipeId } = useParams<{ recipeId?: string }>();
 
-  const { data: recipe, isLoading, error } = useGetRecipeByIdQuery(recipeId as string, {
-    skip: !recipeId,
-  });
+    const {
+        data: recipe,
+        isLoading,
+        error,
+    } = useGetRecipeByIdQuery(recipeId as string, {
+        skip: !recipeId,
+    });
 
-  const { categories } = useCategoryMap()
-  const { categoryTitles } = getCategoryPairFromRecipe(recipe as Recipe, categories || [])
+    const { categories } = useCategoryMap();
+    const { categoryTitles } = getCategoryPairFromRecipe(recipe as Recipe, categories || []);
 
-  if (isLoading) return <Spinner />;
-  if (error || !recipe) return <Error />;
+    if (isLoading) return <Spinner />;
+    if (error || !recipe) return <Error />;
 
+    return (
+        <Box {...recipePageStyle}>
+            {/* картинка + описание */}
+            <Box {...pictureDescriptionBoxStyle}>
+                <Image src={BASE_URL + recipe.image} alt={recipe.title} {...imageRecipeStyle} />
 
-  return (
-    <Box {...recipePageStyle}>
-      {/* картинка + описание */}
-      <Box {...pictureDescriptionBoxStyle}>
-        <Image src={BASE_URL + recipe.image} alt={recipe.title} {...imageRecipeStyle} />
+                <Box {...descriptionBoxStyle}>
+                    {/* категории + иконки*/}
+                    <Box {...categoriesIconsStyle}>
+                        <Box {...recipeCategoriesStyle}>
+                            {categoryTitles.map((title) => (
+                                <Box key={title} {...categoryBoxStyle}>
+                                    {title}
+                                </Box>
+                            ))}
+                        </Box>
 
-      <Box {...descriptionBoxStyle}>
-        {/* категории + иконки*/}
-        <Box {...categoriesIconsStyle}>
+                        <Box {...likeSaveIconsBox}>
+                            <Box {...iconLikeSaveBoxStyle}>
+                                <Image src={saveIcon} alt='icon' {...iconStyle} />
+                                <Text>{recipe.bookmarks}</Text>
+                            </Box>
 
-          <Box {...recipeCategoriesStyle}>
-            {categoryTitles.map((title) => (
-              <Box key={title} {...categoryBoxStyle}>{title}</Box>
-            ))}
-          </Box>
+                            <Box {...iconLikeSaveBoxStyle}>
+                                <Image src={likeIcon} alt='icon' {...iconStyle} />
+                                <Text>{recipe.likes}</Text>
+                            </Box>
+                        </Box>
+                    </Box>
 
-          <Box {...likeSaveIconsBox}>
-            <Box {...iconLikeSaveBoxStyle}>
-              <Image src={saveIcon} alt="icon" {...iconStyle} />
-              <Text >{recipe.bookmarks}</Text>
+                    {/* название рецепта + описание */}
+                    <Box {...titleDescriptionBoxStyle}>
+                        <Text {...titleRecipeStyle}>{recipe.title}</Text>
+                        <Text {...descriptionRecipeStyle}>{recipe.description}</Text>
+                    </Box>
+
+                    {/* кнопки */}
+
+                    <Box {...buttonsBoxStyle}>
+                        <Text {...timeRecipeStyle}>{recipe.time} минут</Text>
+                        <HStack spacing={{ base: '2', md: '2', lg: '4' }}>
+                            <Button
+                                variant='outline'
+                                {...buttonRecipeStyle}
+                                leftIcon={<Icon as={MdSentimentSatisfiedAlt} />}
+                            >
+                                Оценить рецепт
+                            </Button>
+
+                            <Button
+                                backgroundColor='#B1FF2E'
+                                {...buttonRecipeStyle}
+                                leftIcon={<Icon as={BsBookmark} />}
+                            >
+                                Сохранить в закладки
+                            </Button>
+                        </HStack>
+                    </Box>
+                </Box>
             </Box>
-
-            <Box {...iconLikeSaveBoxStyle}>
-              <Image src={likeIcon} alt="icon" {...iconStyle} />
-              <Text >{recipe.likes}</Text>
-            </Box>
-          </Box>
         </Box>
-
-         {/* название рецепта + описание */}
-            <Box {...titleDescriptionBoxStyle}>
-              <Text {...titleRecipeStyle}>{recipe.title}</Text>
-              <Text {...descriptionRecipeStyle}>{recipe.description}</Text>
-            </Box>
-
-          {/* кнопки */}
-
-          
-          </Box>
-      </Box>
-    </Box>
-  );
+    );
 }
 
 export const Component = RecipePage;
--- src/features/recipe/recipe.styles.ts
+++ src/features/recipe/recipe.styles.ts
@@ -1,82 +1,96 @@
 export const recipePageStyle = {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    mt:{base:"16px", md:"18px", lg:"56px"},
-    maxW:{base:"328px", md:"728px", lg:"1360px"}
-} as const 
-
-
-export const pictureDescriptionBoxStyle = {
-    display: "flex",
-    flexDirection: {base:"column", md:"row", lg:"row"},
-    gap:{base:"16px", md:"16px", lg:"24px"},
-} as const 
-
-
-export const descriptionBoxStyle = {
-    display: "flex",
-    flexDirection:"column",
-    h:{base:"344px", md:"224px", lg:"410px"},
-    justifyContent: "space-between",
-} as const
-
-
-export const imageRecipeStyle = {
-    w:{base:"328px", md:"232px", lg:"553px"},
-    h:{base:"224px", md:"224px", lg:"410px"},
-    borderRadius: "8px",
-    background: "rgba(0, 0, 0, 0.08)",
-}
-
-
-export const categoriesIconsStyle = {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems:"start", 
-    w:{base:"328px",md:"480px", lg:"783px"}
-} as const 
-
-
-export const recipeCategoriesStyle = {
-    display: "flex", 
-    flexDirection: { base: "column", md: "row", lg: "row"},
-    gap: {base:"2px", md:"6px",lg:"8px"}, 
-    flexWrap: "wrap", 
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    mt: { base: '16px', md: '18px', lg: '56px' },
+    maxW: { base: '328px', md: '728px', lg: '1360px' },
 } as const;
 
+export const pictureDescriptionBoxStyle = {
+    display: 'flex',
+    flexDirection: { base: 'column', md: 'row', lg: 'row' },
+    gap: { base: '16px', md: '16px', lg: '24px' },
+} as const;
+
+export const descriptionBoxStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    gap: { base: '24px', md: '10px' },
+} as const;
+
+export const imageRecipeStyle = {
+    w: { base: '328px', md: '232px', lg: '553px' },
+    h: { base: '224px', md: '224px', lg: '410px' },
+    borderRadius: '8px',
+    background: 'rgba(0, 0, 0, 0.08)',
+};
+
+export const categoriesIconsStyle = {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'start',
+    w: { base: '328px', md: '480px', lg: '783px' },
+} as const;
+
+export const recipeCategoriesStyle = {
+    display: 'flex',
+    flexDirection: { base: 'column', md: 'row', lg: 'row' },
+    gap: { base: '6px', md: '6px', lg: '8px' },
+    flexWrap: 'wrap',
+} as const;
 
 export const categoryBoxStyle = {
-    display:"inline-block",
-    borderRadius: "4px",   
-    background: "#FFFFD3",
-    py: "1px",
-    px: "4px",
-    fontSize:"14px"
-} 
-
+    display: 'inline-block',
+    borderRadius: '4px',
+    background: '#FFFFD3',
+    py: '1px',
+    px: '4px',
+    fontSize: '14px',
+};
 
 export const titleDescriptionBoxStyle = {
-    display: "flex",
-    flexDirection:"column",
-    gap:{base:"16px", md:"16px", lg:"24px"},
-    w:{base:"328px", md:"480px", lg:"503px"}
-} as const 
-
+    display: 'flex',
+    flexDirection: 'column',
+    gap: { base: '8px', md: '6px', lg: '16px' },
+    w: { base: '328px', md: '480px', lg: '683px' },
+} as const;
 
 export const titleRecipeStyle = {
-    fontSize:{base:"24px", md:"24px", lg:"48px" },
-    fontWeight:"700",
-    lineHeight:{base:"32px", md:"32px", lg:"48px"},
-}
-
+    fontSize: { base: '24px', md: '24px', lg: '48px' },
+    fontWeight: '700',
+    lineHeight: { base: '32px', md: '32px', lg: '48px' },
+};
 
 export const descriptionRecipeStyle = {
-    fontSize:"14px",
-    fontWeight:"400",
-}
+    fontSize: '14px',
+    fontWeight: '400',
+};
 
+export const buttonsBoxStyle = {
+    display: 'flex',
+    flexDirection: { base: 'column', md: 'row', lg: 'row' },
+    justifyContent: 'space-between',
+    alignItems: { base: 'flex-start', md: 'flex-end', lg: 'flex-end' },
+    gap: '12px',
+} as const;
 
+export const timeRecipeStyle = {
+    display: 'inline-flex',
+    borderRadius: '4px',
+    background: 'rgba(0, 0, 0, 0.06)',
+    padding: '2px 6px',
+    w: 'fit-content',
+    fontSize: '14px',
+};
 
-
+export const buttonRecipeStyle = {
+    width: 'fit-content',
+    height: { base: '24px', md: '24px', lg: '36px' },
+    fontSize: { base: '12px', md: '12px', lg: '16px' },
+    fontWeight: 600,
+    lineHeight: { base: '16px', md: '16px', lg: '24px' },
+    px: { base: 2, md: 3, lg: 4 },
+    borderRadius: 'md',
+} as const;
