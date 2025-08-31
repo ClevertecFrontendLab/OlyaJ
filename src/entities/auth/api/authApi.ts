68da8007458a1bb87ed1fcb847f1a6b7f7b0a27f
OlyaJ <schastnayaolga92@gmail.com>
2025-08-31 21:12:29 +0300

    sign up

--- src/entities/auth/api/authApi.ts
+++ src/entities/auth/api/authApi.ts
@@ -2,15 +2,15 @@ import { apiSlice } from '../../../query/create-api';
 import { AuthResponse, SignUp } from '../model/authTypes';
 
 export const authApi = apiSlice.injectEndpoints({
-    endpoints: (builder) => ({
-        signUp: builder.mutation<AuthResponse, SignUp>({
-            query: (body) => ({
-                url: '/auth/signup',
-                method: 'POST',
-                body,
-            }),
-        }),
-    }),
+   endpoints: (builder) => ({
+      signUp: builder.mutation<AuthResponse, SignUp>({
+         query: (body) => ({
+            url: '/auth/signup',
+            method: 'POST',
+            body,
+         }),
+      }),
+   }),
 });
 
 export const { useSignUpMutation } = authApi;
