f7c47ba775bb72119f8f7fbdb9d36a00aa0ed4ab
OlyaJ <schastnayaolga92@gmail.com>
2025-09-25 21:00:05 +0300

    login

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,13 +1,13 @@
 import './index.css';
-
 import { ChakraProvider } from '@chakra-ui/react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
-
 import { router } from './router';
 import { store } from '../store/configure-store';
+
+
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
@@ -16,5 +16,5 @@ createRoot(document.getElementById('root')!).render(
                 <RouterProvider router={router} />
             </Provider>
         </ChakraProvider>
-    </StrictMode>,
+    </StrictMode>
 );
